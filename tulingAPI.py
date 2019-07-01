9487d060b52eaa5761876f7b43de1fd030d7cdd0
rdhuht <xinhuaxilu6@gmail.com>
2019-07-01 10:46:53 +0800

    增加注释

--- tulingAPI.py
+++ tulingAPI.py
@@ -11,29 +11,30 @@ from mcpi import minecraft
 APIkey = '85fa9349884747f5b72913a7c60bbcab'
 opList = ["Hy", "IvyGao", "JeremyTsui", "momo", "Ningliang"]
 
-# address = '47.105.46.254'
-address = '101.200.42.193'
+address = '47.105.46.254'
+# address = '101.200.42.193'
 # address = 'localhost'
 mc = minecraft.Minecraft.create(address=address)
 
 
-def get_response(msg):
+def get_response(text_input):
     api_url = "http://openapi.tuling123.com/openapi/api/v2"
-    data = {
-        "perception":
+    req = {
+        "reqType": 0,  # 输入类型 0-文本, 1-图片, 2-音频
+        "perception":  # 信息参数
             {
-                "inputText":
+                "inputText":  # 文本信息
                     {
-                        "text": msg
+                        "text": text_input
                     },
 
-                "selfInfo":
+                "selfInfo":  # 用户参数
                     {
                         "location":
                             {
-                                "city": "Beijing",
-                                "province": "Beijing",
-                                "street": "GuangquRoad"
+                                "city": "Beijing",  # 所在城市
+                                "province": "Beijing",  # 省份
+                                "street": "GuangquRoad"  # 街道
                             }
                     }
             },
@@ -41,11 +42,11 @@ def get_response(msg):
         "userInfo":
             {
                 "apiKey": "85fa9349884747f5b72913a7c60bbcab",
-                "userId": "OnlyUseAlphabet"
+                "userId": "007"  # # 用户唯一标识(随便填, 非密钥)
             }
     }
     try:
-        req = json.dumps(data).encode('utf-8')
+        req = json.dumps(req).encode('utf-8')  # dict->utf-8
         http_post = urllib.request.Request(api_url, data=req, headers={'content-type': 'application/json'})
         response = urllib.request.urlopen(http_post)
         response_str = response.read().decode('utf-8')
@@ -58,8 +59,6 @@ def get_response(msg):
 
 def tuling_reply_text():
     for chatpost in mc.events.pollChatPosts():
-        print(chatpost)
-        print(type(chatpost.message))
         playername = mc.entity.getName(chatpost.entityId)
         if playername in opList:
             ms = chatpost.message.lower()
@@ -78,7 +77,6 @@ def tuling_reply_text():
                 mc.postToChat('ministack AI: ' + r)
 
 
-# 你好
 while True:
     time.sleep(0.5)
     tuling_reply_text()
